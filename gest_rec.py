871218e765085bd715b37e93eff8ee027e5abf59
rak94 <62082190+rak94@users.noreply.github.com>
2020-04-28 20:53:00 -0500

    initial commit

---
+++ gest_rec.py
@@ -0,0 +1,89 @@
+import cv2
+import numpy as np
+import wx
+from pynput.mouse import Button, Controller
+mouse = Controller()
+
+application = wx.App(False)
+(sx, sy) = wx.GetDisplaySize()
+(camx, camy) = (320, 240)
+
+lowerBound = np.array([33, 80, 40])
+upperBound = np.array([102, 235, 255])
+
+camera = cv2.VideoCapture(0)
+camera.set(3, camx)
+camera.set(4, camy)
+kernelOpen = np.ones((5, 5))
+kernelClose = np.ones((20, 20))
+
+# mouse dampening
+mLocOld = np.array([0, 0])
+mouseLoc = np.array([0, 0])
+DampingFactor = 2
+
+# pinch gesture
+pinchFlag = 0
+openx, openy, openw, openh = (0, 0, 0, 0)
+
+while True:
+    ret, img = camera.read()
+
+    # converting BGR to HSV
+    imageHSV = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+
+    #  creating the mask
+    mask = cv2.inRange(imageHSV, lowerBound, upperBound)
+
+    #  morphing the image
+    maskOpen = cv2.morphologyEx(mask, cv2.MORPH_OPEN, kernelOpen)
+    maskClose = cv2.morphologyEx(mask, cv2.MORPH_CLOSE, kernelClose)
+
+    maskFinal = maskClose
+    contours, h = cv2.findContours(maskFinal.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
+
+    cv2.drawContours(img, contours, -1, (255, 0, 0), 3)
+
+    if(len(contours) == 2):
+        if(pinchFlag == 1):
+            pinchFlag = 0
+            mouse.release(Button.left)
+        x1, y1, w1, h1 = cv2.boundingRect(contours[0])
+        x2, y2, w2, h2 = cv2.boundingRect(contours[1])
+        cv2.rectangle(img, (x1, y1), (x1+y1), (w1+h1), (255, 0, 0), 2)
+        cv2.rectangle(img, (x2, y2), (x2+y2), (w2+h2), (255, 0, 0), 2)
+        cx1 = x1 + w1 / 2
+        cy1 = y1 + h1 / 2
+        cx2 = x2 + w2 / 2
+        cy2 = y2 + h2 / 2
+        cx = (cx1 + cx2) / 2
+        cy = (cy1 + cy2) / 2
+        cv2.line(img, (cx1, cy1), (cx2, cy2), (255, 0, 0), 2)
+        cv2.circle(img, (cx, cy), 2, (0, 0, 255), 2)
+        mouseLoc = mLocOld + ((cx, cy) - mLocOld) / DampingFactor
+        mouse.position = (sx - (mouseLoc[0] * sx / camx), mouseLoc[1] * sy / camy)
+        while mouse.position != (sx - (mouseLoc[0] * sx / camx), mouseLoc[1] * sy / camy):
+            pass
+        mLocOld = mouseLoc
+        openx, openy, openw, openh = cv2.boundingRect(np.array([[[x1, y1], [x1 + w1, y1 + h1], [x2, y2], [x2 + w2, y2 + h2]]]))
+
+    elif (len(contours) == 1):
+        x, y, w, h = cv2.boundingRect(contours[0])
+        if(pinchFlag ==0):
+            if(abs((w * h - openx * openh)*100 / (w * h)) < 30):
+                pinchFlag = 1
+                mouse.press(Button.left)
+                openx, openy, openw, openh = (0, 0, 0, 0)
+        else:
+            cv2.rectangle(img, (x,y), (x + w), (y + h), (255, 0, 0), 2)
+            cx = x + w / 2
+            cy = y + h / 2
+            cv2.circle(img, (cx, cy), (w + h) / 4, (0, 0, 255), 2)
+            mouseLoc = mLocOld + ((cx, cy) - mLocOld) / DampingFactor
+            mouse.position = (sx - (mouseLoc[0] * sx / camx), mouseLoc[1] * sy / camy)
+            while mouse.position != (sx - (mouseLoc[0] * sx / camx), mouseLoc[1] * sy / camy):
+                pass
+            mLocOld = mouseLoc
+
+    cv2.imshow("camera", img)
+    cv2.waitKeyEx(6)
